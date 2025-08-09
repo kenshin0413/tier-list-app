d55a24da6121da9b97ec49a644aad2b327cf2146
kenshin0413 <kenshin1234569@gmail.com>
2025-08-09 16:49:33 +0900

    初期

---
+++ tier-list-app/tier-list-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tier-list-app
+//
+//  Created by miyamotokenshin on R 7/08/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tier-list-app/tier-list-app/tier_list_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  tier_list_appApp.swift
+//  tier-list-app
+//
+//  Created by miyamotokenshin on R 7/08/09.
+//
+
+import SwiftUI
+
+@main
+struct tier_list_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
